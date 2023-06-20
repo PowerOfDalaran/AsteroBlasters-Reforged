e93753a6a8dc8c8fea315a689ed1dcbe18888754
Piotr Kluczynski <peklucz@gmail.com>
2023-06-21 00:04:08 +0200

    Added simple singleton responsible for changing scenes (requires further upgrades), script for handling button functionalities and first UI elements for test scene.

---
+++ AsteroBlasters-Reforged/Assets/Scripts/Singletons/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Singleton responsible for changing scenes. It can be reached by its static property <c>instance</c>.
+/// </summary>
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    private void Awake()
+    {
+        // Checking if another instance of this class don't exist yet and deleting itself if that is the case
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Method changing scene to one with given build index.
+    /// Needs further upgrades - use the <c>progress</c> parameter to create progress bar?
+    /// </summary>
+    /// <param name="sceneId">Build index of the scene to be loaded</param>
+    public async void LoadScene(int sceneId)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneId);
+        asyncOperation.allowSceneActivation = false;
+
+        do
+        {
+            await Task.Delay(100);
+        } while (asyncOperation.progress < 0.9f);
+
+        asyncOperation.allowSceneActivation = true;
+    }
+}
