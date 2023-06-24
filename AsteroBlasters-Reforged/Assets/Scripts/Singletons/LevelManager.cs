4822649283260348ec63a16ae06ddf1a373a89d8
Piotr Kluczynski <peklucz@gmail.com>
2023-06-24 11:58:26 +0200

    Added simple animation for exiting and entering new scene.

--- AsteroBlasters-Reforged/Assets/Scripts/Singletons/LevelManager.cs
+++ AsteroBlasters-Reforged/Assets/Scripts/Singletons/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Singleton responsible for changing scenes. It can be reached by its static property <c>instance</c>.
@@ -30,6 +31,30 @@ public class LevelManager : MonoBehaviour
     /// <param name="sceneId">Build index of the scene to be loaded</param>
     public async void LoadScene(int sceneId)
     {
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneId);
+        asyncOperation.allowSceneActivation = false;
+
+        do
+        {
+            await Task.Delay(100);
+        } while (asyncOperation.progress < 0.9f);
+
+        asyncOperation.allowSceneActivation = true;
+    }
+
+    /// <summary>
+    /// Method changing scene to one with given build index.
+    /// Needs further upgrades - use the <c>progress</c> parameter to create progress bar?
+    /// </summary>
+    /// <param name="sceneId">Build index of the scene to be loaded</param>
+    public async void LoadScene(int sceneId, Animator transition)
+    {
+
+        transition.SetTrigger("LeaveScene");
+        await Task.Delay(2000);
+
+
         AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneId);
         asyncOperation.allowSceneActivation = false;
 
