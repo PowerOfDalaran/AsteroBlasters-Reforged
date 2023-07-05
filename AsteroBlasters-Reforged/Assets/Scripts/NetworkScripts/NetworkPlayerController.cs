48fc34dc33b27c4adb21221ec7aed1ab3b29e39e
krisiol23 <49209858+krisiol23@users.noreply.github.com>
2023-07-05 19:18:35 +0200

    Change to serverrpc

--- AsteroBlasters-Reforged/Assets/Scripts/NetworkScripts/NetworkPlayerController.cs
+++ AsteroBlasters-Reforged/Assets/Scripts/NetworkScripts/NetworkPlayerController.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.InputSystem;
 
+
+
 /// <summary>
 /// Class responsible for controlling the player character, by moving it, activating sound effects, animations etc.
 /// This version is also using multiple Netcode methods to allow playing in multiplayer mode.
@@ -19,53 +21,56 @@ public class NetworkPlayerController : NetworkBehaviour, IHealthSystem
 
     public NetworkVariable<int> maxHealth = new NetworkVariable<int>();
     public NetworkVariable<int> currentHealth = new NetworkVariable<int>();
+    //public NetworkVariable<float> impactVelocity = new NetworkVariable<float>();
 
-    // Start position for each player
-    private Vector2 startPosition;
-
-    private void Awake()
+    void Awake()
     {
+        // Assigning values to properties
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myPlayerControls = new PlayerControls();
         myWeapon = GetComponent<NetworkWeapon>();
         maxHealth.Value = 3;
-        currentHealth.Value = maxHealth.Value;
-
-        // Store the start position of the player
-        startPosition = myRigidbody2D.position;
+        currentHealth = maxHealth;
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
+        // Adding methods to PlayerControls delegates and activating it
         myPlayerControls.Enable();
         myPlayerControls.PlayerActions.Shoot.performed += Shoot;
     }
 
-    private void OnDisable()
+    void OnDisable()
     {
+        // Removing methods from PlayerControls delegates and deactivating it
         myPlayerControls.Disable();
         myPlayerControls.PlayerActions.Shoot.performed -= Shoot;
     }
 
     private void FixedUpdate()
     {
+        // Deciding whether the rest of method should be activated
         if (!IsOwner)
         {
             return;
         }
         CameraController.instance.FollowPlayer(transform);
 
+
+        // Reading current input value for movement and if it's different than zero activate movement and rotation
         Vector2 movementVector = myPlayerControls.PlayerActions.Move.ReadValue<Vector2>();
 
-        if (!movementVector.Equals(Vector2.zero))
+        if (!movementVector.Equals(new Vector2(0, 0)))
         {
             Movement(movementVector);
             Rotate(movementVector);
         }
 
+        // Checking if the player is still alive
         if (currentHealth.Value <= 0)
         {
-            Die();
+            //Debug.Log(currentHealth);
+
         }
     }
 
@@ -90,11 +95,15 @@ public class NetworkPlayerController : NetworkBehaviour, IHealthSystem
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
+        // Checking if collision damage should be applied and applying it
         if (!collision.gameObject.CompareTag("NoImpactDamage"))
         {
             if (!IsOwner) return;
             if (collision.gameObject.TryGetComponent(out NetworkPlayerController networkPlayer))
             {
+                //impactVelocity.Value = collision.relativeVelocity.magnitude;
+
                 var player1 = new PlayerData()
                 {
                     Id = OwnerClientId,
@@ -105,10 +114,12 @@ public class NetworkPlayerController : NetworkBehaviour, IHealthSystem
                 {
                     Id = networkPlayer.OwnerClientId,
                     ImpactVelocity = collision.relativeVelocity.magnitude
-                };
 
+
+                };
                 ImpactDamageServerRpc(player1, player2);
             }
+
         }
     }
 
@@ -124,8 +135,14 @@ public class NetworkPlayerController : NetworkBehaviour, IHealthSystem
         }
     }
 
+    /// <summary>
+    /// Method activating the current weapon in order to fire.
+    /// Is added to the "PlayerActions.Move.performed" delegate.
+    /// </summary>
+    /// <param name="context">Value gathered by input system</param>
     void Shoot(InputAction.CallbackContext context)
     {
+        // Deciding whether the rest of method should be activated
         if (!IsOwner)
         {
             return;
@@ -133,51 +150,72 @@ public class NetworkPlayerController : NetworkBehaviour, IHealthSystem
         myWeapon.ShootServerRpc();
     }
 
+    /// <summary>
+    /// Method moving player character by adding force to its rigidbody2D component.
+    /// Is triggered in "FixedUpdate()" method each frame.
+    /// </summary>
+    /// <param name="context">Value gathered by input system</param>
     void Movement(Vector2 movementVector)
     {
         myRigidbody2D.AddForce(movementVector * movementSpeed, ForceMode2D.Force);
     }
 
+    /// <summary>
+    /// Method rotating player character by creating new desired rotation and then using it to calculate rotation.
+    /// Is triggered in "FixedUpdate()" method each frame.
+    /// Not that proud of the result, may look for better rotation system later.
+    /// </summary>
+    /// <param name="movementVector">Value gathered by input system</param>
     void Rotate(Vector2 movementVector)
     {
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, movementVector);
         Quaternion newRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
-        transform.rotation = newRotation;
+        gameObject.transform.rotation = newRotation;
     }
 
     public void TakeDamage(int damage)
     {
-        if (currentHealth.Value > 0)
+        if(IsClient || IsOwner)
         {
-            currentHealth.Value -= damage;
+            TakeDamageServerRpc(damage);
+        }
+        
+    }
+
+    [ServerRpc (RequireOwnership = false)]
+    public void TakeDamageServerRpc(int damage, ServerRpcParams serverRpcParams = default)
+    {
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        var client = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.GetComponent<NetworkPlayerController>();
+
+        if (client.currentHealth.Value > 0)
+        {
+            client.currentHealth.Value -= damage;
         }
         else
         {
-            Die();
+            client.currentHealth.Value = 3;
+            DieServerRpc();
         }
-        Debug.Log("You took " + damage + " damage! You have " + currentHealth.Value);
+        Debug.Log("You took " + damage + " damage!, You have " + client.currentHealth.Value);
     }
 
     public void Die()
     {
         Debug.Log("You died");
 
-        // Reset the position of the player to the start position
-        myRigidbody2D.position = startPosition;
-        myRigidbody2D.velocity = Vector2.zero;
+        //DieServerRpc();
 
-        // Reset the health
-        currentHealth.Value = maxHealth.Value;
-
-        if (IsOwner)
-        {
-            DieServerRpc();
-        }
     }
 
     [ServerRpc]
     public void DieServerRpc(ServerRpcParams serverRpcParams = default)
     {
-        Debug.Log("You are dead " + OwnerClientId);
+        var clientId = serverRpcParams.Receive.SenderClientId;
+        Debug.Log("You are dead " + clientId);
+        var client = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject.GetComponent<NetworkPlayerController>();
+        client.myRigidbody2D.position = new Vector2(0f, 0f);
+        client.myRigidbody2D.velocity = Vector2.zero;
+        Debug.Log(client.myRigidbody2D.position);
     }
 }
