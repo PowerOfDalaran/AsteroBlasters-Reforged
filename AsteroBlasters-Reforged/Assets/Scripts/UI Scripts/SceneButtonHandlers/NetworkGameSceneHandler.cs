6982d7b0b437e79c70fe7199483c835edae69873
Piotr Kluczynski <peklucz@gmail.com>
2023-08-08 14:36:05 +0200

    Created simple timer, presenting how long the game will last and added new ui elements for further development.

--- AsteroBlasters-Reforged/Assets/Scripts/UI Scripts/SceneButtonHandlers/NetworkGameSceneHandler.cs
+++ AsteroBlasters-Reforged/Assets/Scripts/UI Scripts/SceneButtonHandlers/NetworkGameSceneHandler.cs
@@ -1,13 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetworkGameSceneHandler : MonoBehaviour
 {
+    [SerializeField]
+    Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
         MultiplayerGameManager.instance.StartTheGame();
+    }
+
+    private void Update()
+    {
+        TimeSpan timerTimeSpan = TimeSpan.FromSeconds(DeathmatchGameManager.instance.timeLeft.Value);
+
+        string minutesLeft = timerTimeSpan.Minutes < 10 ? "0" + timerTimeSpan.Minutes.ToString() : timerTimeSpan.Minutes.ToString();
+        string secondsLeft = timerTimeSpan.Seconds < 10 ? "0" + timerTimeSpan.Seconds.ToString() : timerTimeSpan.Seconds.ToString();
+
+        timerText.text = minutesLeft + ":" + secondsLeft;
     }
 }
