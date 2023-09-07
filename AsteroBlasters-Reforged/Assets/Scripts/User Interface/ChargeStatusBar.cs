b90b896b82164278a8751cb5c83a774d476e0400
Piotr Kluczynski <peklucz@gmail.com>
2023-09-07 12:28:58 +0200

    Short commit with documentation to the new script .

--- AsteroBlasters-Reforged/Assets/Scripts/User Interface/ChargeStatusBar.cs
+++ AsteroBlasters-Reforged/Assets/Scripts/User Interface/ChargeStatusBar.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 namespace UserInterface
 {
+    /// <summary>
+    /// Class managing the UI slider, displaying the charged power of current shot 
+    /// </summary>
     public class ChargeStatusBar : MonoBehaviour
     {
         [SerializeField] Slider slider;
@@ -18,6 +21,10 @@ namespace UserInterface
             PlayerController.onChargeValueChanged -= UpdateChargeBar;
         }
 
+        /// <summary>
+        /// Method adjusting the slider to proper value
+        /// </summary>
+        /// <param name="value">Value, which slider should display</param>
         void UpdateChargeBar(float value)
         {
             slider.value = value;
